21e721e5cae50442bc997d92731aec0b8197d7f8
thiago-fiori <thiagofiori12@gmail.com>
2020-03-27 10:25:22 -0300

    function

--- curso_js/script.js
+++ curso_js/script.js
@@ -1,43 +1,5 @@
-/* Variáveis e condicionais
-
-var a = 2;
-var b = 3;
-var soma = a + b;
-var sub = a - b;
-var multi = a * b;
-
-if (b % 2 == 1) {
-  alert("Número Ímpar")
-} else {
-  alert("Número par")
-} */
-
-/*
-Estrutura de repetição
-//while
-var i = 0;
-while (i < 3) {
-  alert(i);
-  i++;
+function soma(a , b){
+  console.log(a + b);
 }
 
-//for
-for (var j = 0; j < 3; j++) {
-  alert(j);
-}
-*/
-
-
-/*
-Tads
-var numero = 6;
-var decimal = 6.5;
-var texto = "hello world";
-var lista = ["banana", "mamao", "laranja"];
-
-for (var i = 0; i < lista.length; i++) {
-lista[i];
-}
-*/
-
-console.log("Olá");
+soma(2,2);
