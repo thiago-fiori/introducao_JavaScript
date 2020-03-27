9926a89c0b8553609679e01f05aed49926409d01
thiago-fiori <thiagofiori12@gmail.com>
2020-03-27 10:14:14 -0300

    estrutura de repetição

--- curso_js/script.js
+++ curso_js/script.js
@@ -1,11 +1,20 @@
-var a = 2;
-var b = 3;
-var soma = a + b;
-var sub = a - b;
-var multi = a * b;
+// Variáveis e condicionais
 
-if (b % 2 == 1) {
-  alert("Número Ímpar")
-} else {
-  alert("Número par")
+// var a = 2;
+// var b = 3;
+// var soma = a + b;
+// var sub = a - b;
+// var multi = a * b;
+//
+// if (b % 2 == 1) {
+//   alert("Número Ímpar")
+// } else {
+//   alert("Número par")
+// }
+
+// Estrutura de repetição
+var i = 0;
+while (i < 3) {
+  alert(i);
+  i++;
 }
