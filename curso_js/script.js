6288ee92e31ab30f25cfd22f4a07912766b2f0f6
thiago-fiori <thiagofiori12@gmail.com>
2020-03-27 11:03:57 -0300

    adicionado botão de mudar estilos e um botão para limpar os estilos

--- curso_js/script.js
+++ curso_js/script.js
@@ -1,4 +1,24 @@
 function mudaCor(cor){
   var elemento = document.getElementById("mensagem");
-  elemento.style.color = cor;
+  elemento.style.backgroundColor = cor;
+}
+
+function aumenta(){
+  var mudado = document.getElementById("tamanho");
+  mudado.style.fontSize = "100px";
+}
+
+function diminui(){
+  var elemento = document.getElementById("tamanho");
+  elemento.style.fontSize = "10px";
+}
+
+function limpa_tamanho(){
+  var elemento = document.getElementById("tamanho");
+  tamanho.style = "clear";
+}
+
+function limpa_color(){
+  var elemento = document.getElementById("mensagem");
+  mensagem.style = "clear";
 }
